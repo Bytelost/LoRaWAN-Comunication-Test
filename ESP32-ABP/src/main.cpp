5198ab53068136c76288099624ed3c4fedb3b822
Vinicius Muller de Freitas <viniciusmullerdefreitas309@gmail.com>
2023-03-19 19:52:32 -0300

    fea: see if conection is stablished

---
+++ ESP32-ABP/src/main.cpp
@@ -0,0 +1,21 @@
+#include "heltec.h"
+#include <Arduindo.h>
+
+void setup() { 
+   //WIFI Kit series V1 not support Vext control
+  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.Heltec.Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
+ 
+  Heltec.display->init();
+  Heltec.display->setFont(ArialMT_Plain_10);
+  delay(1500);
+  Heltec.display->clear();
+  
+  Heltec.display->drawString(0, 0, "LoRa Iniciado!");
+  Heltec.display->drawString(0, 10, "Esprendo Dados...");
+  Heltec.display->display();
+  delay(1000);
+  //LoRa.onReceive(cbk);
+  LoRa.receive();
+}
+
+
