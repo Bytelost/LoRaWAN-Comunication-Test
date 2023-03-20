4f01aca72a9d949b05ecf2a8f96f9c84d06a5233
Vinicius Muller de Freitas <viniciusmullerdefreitas309@gmail.com>
2023-03-19 22:07:55 -0300

    feat: try ABP connection

--- ESP32-ABP/src/main.cpp
+++ ESP32-ABP/src/main.cpp
@@ -1,21 +1,24 @@
 #include "heltec.h"
 #include <Arduino.h>
+#include <lmic.h>
 
-void setup() { 
-   //WIFI Kit series V1 not support Vext control
-  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.Heltec.Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
- 
+void setup ()
+{
+  Heltec.begin(true);
+
   Heltec.display->init();
+  Heltec.display->flipScreenVertically();  
   Heltec.display->setFont(ArialMT_Plain_10);
   delay(1500);
   Heltec.display->clear();
   
-  Heltec.display->drawString(0, 0, "LoRa Iniciado!");
-  Heltec.display->drawString(0, 10, "Esprendo Dados...");
+  Heltec.display->drawString(0, 0, "Heltec.LoRa Initial success!");
+  Heltec.display->drawString(0, 10, "Wait for incoming data...");
   Heltec.display->display();
   delay(1000);
-  //LoRa.onReceive(cbk);
-  LoRa.receive();
 }
 
-
+void loop()
+{
+  delay(10);
+}
